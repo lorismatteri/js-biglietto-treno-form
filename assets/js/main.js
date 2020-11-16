ab1c9977de49a6afa389b01147501fa223757816
Loris Matteri <loris.matteri93@gmail.com>
2020-11-16 18:27:21 +0100

    CSS
    Aggiunta estetica in css

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,28 +4,21 @@ var btnAnnulla = document.getElementById('btn_annulla');
 btnGenera.addEventListener("click", function () {
   var offer = "Standard Ticket"
 
-  //Genera ticket
-  console.log("genera il ticket");
-
+  //Genera ticket:
   // selezionare valore input
   var fullNameInput = document.getElementById("full_name");
-
   var kmInput = document.getElementById('km');
-
   var ageInput = document.getElementById('age');
-
 
   // Seleziona i valori
   var fullName = fullNameInput.value;
   var km = kmInput.value;
   var age = ageInput.value;
-  console.log(fullName, km, age);
 
   // calcolo biglietto treno
   var sconto;
   var costPerKm = 0.21;
   var ticketPrice = costPerKm * km;
-  console.log(ticketPrice);
 
   if (age == "minorenne") {
     sconto = ticketPrice * 20 / 100;
@@ -51,23 +44,21 @@ btnGenera.addEventListener("click", function () {
   // Inserire dati nel biglietto
   elName.innerHTML = fullName;
   elOfferta.innerHTML = offer;
-  elTicket.innerHTML = "€" + ticketPrice;
+  elTicket.innerHTML = "€" + ticketPrice.toFixed(2);
   elCarrozza.innerHTML = numberCarrozza;
   elCodice.innerHTML = codiceCp
 
-  document.getElementById('biglietto').style.display = 'block';
+  document.getElementById('biglietto').style.opacity = '1';
 });
 
 btnAnnulla.addEventListener("click", function () {
   //Annulla
   console.log("annulla operazione");
 
-  document.getElementById('biglietto').style.display = 'none';
+  document.getElementById('biglietto').style.opacity = '0';
 
   var fullNameInput = document.getElementById("full_name");
-
   var kmInput = document.getElementById('km');
-
   var ageInput = document.getElementById('age');
 
   fullNameInput.value = "";
